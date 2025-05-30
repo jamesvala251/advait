0143f204bf7ae44fa4cf7798535aac04b7521faf
Jashvant <jashvant.vala@gmail.com>
2025-05-30 12:03:13 +0530

    ipset

--- advait-backend/config/cors.php
+++ advait-backend/config/cors.php
@@ -1,15 +1,11 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:3001'
-    ],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_methods' => ['*'],
+'allowed_origins' => ['*'], // or ['http://56.228.54.15']
+'allowed_headers' => ['*'],
+'exposed_headers' => [],
+'max_age' => 0,
+'supports_credentials' => false,
 ];
