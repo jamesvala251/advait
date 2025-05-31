fdb25d6f6a15c0a280c77eaff0024bc1456a806c
Jashvant <jashvant.vala@gmail.com>
2025-05-31 17:25:52 +0530

    api

--- advait-backend/config/cors.php
+++ advait-backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 'allowed_origins' => [
     'http://localhost:3000',
     'http://localhost',
-    'http://56.228.54.15'
+    'http://56.228.54.15:8000'
 ],
 'allowed_headers' => ['*'],
 'exposed_headers' => [],
