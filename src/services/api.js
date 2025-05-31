e88afc3108dae81148cd402fbded9e54638071ad
Jashvant <jashvant.vala@gmail.com>
2025-05-31 15:15:52 +0530

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost/advait/advait-backend/public/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost/advait-backend/public/api';
+
 
 const api = axios.create({
     baseURL: API_URL,
